4b846ee9146f861e4557202ba4fdb1b5245b2146
Lika Sizih <likasizih@mail.ru>
2022-09-29 15:03:47 +0300

    Added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+void LineNumbers(int numberN, int pow)
+{
+    int i = 1;
+    string outline = string.Empty;
+    while (i <= numberN)
+    {
+        Console.Write("{0,7}", Math.Pow(i,pow));
+        i++;
+    }
+    Console.WriteLine();
+}
+
+int num = ReadData("Введите число N: ");
+
+LineNumbers(num,1);
+LineNumbers(num,3);
