0fc62016c2ccd22821d2d1cee871a7680f469e0f
Lika Sizih <likasizih@mail.ru>
2022-09-29 15:03:32 +0300

    Added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿int InputIntNumber()
+{
+    while(true)
+    {
+        Console.WriteLine("Введите пятизначное число: ");
+        int number = int.Parse(Console.ReadLine() ?? "0");
+        while(number > 999 && number < 100000)
+        return number;
+    }
+} 
+int num = InputIntNumber();
+
+void Polindr()
+{
+    string chars = num.ToString();
+    if(chars[0] == chars[4] && chars[1] == chars[3])
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
+Polindr();
